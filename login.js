4881ea7a3db2b67f19d70d0d64a35a4e15c681ec
AND3FELIPE <afsilva6236@misena.edu.co>
2023-05-22 14:09:01 -0500

    Implementa el formulario del ejercicio de html

---
+++ login.js
@@ -0,0 +1,7 @@
+const myButton = document.getElementById("button")
+
+myButton.addEventListener("click", () => {
+    window.location.href="https://fundamentos.makeitreal.camp/login"
+});
+
+// document.getElementById("button").addEventListener("click", function(){window.location.replace("./ejercicio-html-y-css/index.html")});
